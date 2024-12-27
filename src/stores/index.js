4ac83dcd1481ee91ce90ed7b9db21ca36746270f
minyoung kim <ymkved@gmail.com>
2024-12-27 15:49:59 +0900

    fix : 로그인 관련 기능 추가.

--- src/stores/index.js
+++ src/stores/index.js
@@ -9,7 +9,87 @@ function setArticleContent(){}
 function setComments(){}
 function setAuth(){}
 function setArticlesMode(){}
-function setIsLogin(){}
+
+function setIsLogin(){
+    let initValues = {
+        id: '',
+        email: '',
+        Authorization: '' // access_token
+    }
+    
+    const { subscribe, set, update } = writable({...initValues});
+
+    const refresh = async () => {
+        try{
+            const authenticationUser = await postApi({path: '/auth/refresh'});
+            set(authenticationUser);    
+            isRefresh.set(true);            
+        }catch(error){
+            auth.resetUserInfo();
+            isRefresh.set(false);
+        }
+    }
+    const resetUserInfo = () =>{
+        set({...initValues});
+    }
+    const login = async (email, password) => {
+        try{
+            const options = {
+                path : '/auth/login',
+                data : {
+                    email : email,
+                    password : password
+                }
+            }
+            const result = await postApi(options);
+            set(result);
+            isRefresh.set(true);
+            router.goto('/articles');
+        }catch(error){
+            alert("오류가 발생하였습니다. 로그인을 다시시도해주세요.");
+        }
+
+    }
+    const logout = async () => {
+        try{
+            const options = {
+                path : '/auth/logout',
+            }
+            await delApi(options);
+            set({...initValues});
+            isRefresh.set(false);
+            router.goto('/');
+        }catch(error){
+            alert("오류가 발생하였습니다. 다시시도해 주세요.");
+        }
+    }
+    const register = async (email, pwd) => {
+        try{
+            const options = {
+                path : '/auth/login',
+                data : {
+                    email : email,
+                    pwd : pwd
+                }
+            }
+            const result = await postApi(options);
+            alert("가입이 완료되었습니다.");
+            router.goto('/');
+        }catch(error){
+            alert("오류가 발생하였습니다. 로그인을 다시시도해주세요.");
+        }
+    }
+
+    return {
+        subscribe,
+        refresh,
+        resetUserInfo,
+        login,
+        logout,
+        register
+    }
+
+}
 
 export const currentArticlesPage = setCurrentArticlesPage()
 export const articles = setArticles()
@@ -19,3 +99,4 @@ export const comments = setComments()
 export const auth = setAuth()
 export const articlesMode = setArticlesMode()
 export const isLogin = setIsLogin();
+export const isRefresh = writable(false);
